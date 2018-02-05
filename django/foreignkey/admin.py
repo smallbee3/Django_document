4414b0dd4c025957f511ff1d22ef53faeba35b5a
smallbee <smallbee3@gmail.com>
2018-02-05 15:14:25 +0900

    foreignkey field에서 primary_key를 따로 지정한 경우 실습

--- django/foreignkey/admin.py
+++ django/foreignkey/admin.py
@@ -4,3 +4,9 @@ from .models import *
 
 admin.site.register(Manufacturer)
 admin.site.register(Car)
+
+admin.site.register(Person)
+
+admin.site.register(Type)
+admin.site.register(Pokemon)
+
--- django/foreignkey/models.py
+++ django/foreignkey/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 
-class Manufacturer(models.Model):
-    # ...
-    name = models.CharField('제조사명', max_length=50)
 
-    def __str__(self):
-        return self.name
 
 class Car(models.Model):
     manufacturer = models.ForeignKey(
-        Manufacturer,
+
+        'Manufacturer', # -> 문자열로 하면 참조하기 때문에 가능.
         on_delete=models.CASCADE,
         verbose_name='제조사',
     )
@@ -17,3 +13,45 @@ class Car(models.Model):
 
     def __str__(self):
         return f'{self.manufacturer.name} {self.name}'
+
+class Manufacturer(models.Model):
+    name = models.CharField('제조사명', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=60)
+    # 자기 자신을 다대일로 연결하는 필드
+    # 비어있어도 무관, 연결된 객체가 삭제되면 함께 삭제되지 않고
+    # 해당 필드를 비움
+    teacher = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL, # 선생이 삭제되었을 때 어떻게 할 것인지.
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        if self.teacher:
+            return f'{self.name} (teacher: {self.teacher})'
+        return f'{self.name}'
+
+
+
+# class Type(models.Model):
+#     name = models.CharField(primary_key=True, max_length=60)
+#
+#     def __str__(self):
+#         return f'{self.type_numbeer}| {self.name}'
+#
+#
+# class Pokemon(models.Model):
+#
+#     dex_number = models.IntegerField(primary_key=True)
+#     type = models.ForeignKey(Type, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#
+#     def __str__(self):
+#         return f'{self.dex_number:03}. {self.name} (self.type.name)'
